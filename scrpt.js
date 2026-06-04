c4b71a171900e911ba2c031ddf631ae576714795
dana citlali <danaluna863@gmail.com>
2026-06-03 20:52:21 -0600

    Subida definitiva desde XAMPP

---
+++ scrpt.js
@@ -0,0 +1,9 @@
+// Mensaje al presionar botón
+document.getElementById('botonSaludo').addEventListener('click', function() {
+    alert("¡Excelente! México vivirá una gran fiesta en 2026 🇲🇽⚽");
+});
+
+// Mensaje de bienvenida al cargar la página
+window.onload = function() {
+    console.log("Sitio cargado correctamente. Listo para el Mundial 2026.");
+};
